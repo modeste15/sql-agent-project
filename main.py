9fda98999f26cc7137b49c7d741eb34a0b0cc412
modeste <medouse15@gmail.com>
2026-04-21 07:13:33 +0200

    Input ok in terminal

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ system_prompt = "You are a helpful assistant that answers questions in a concise
 
 
 class Conversation : 
-    def __init__ (self, client, model) :
+    def __init__ (self) :
         self.client = client
         self.model = model
         self.temperature = temperature
@@ -22,31 +22,58 @@ class Conversation :
 
     def new_message (self, message: str) :
         self.conversation_history.append({"role": "user", "content": message})
-        chat_response = self.client.chat.completions.create(
+
+        chat_response = self.client.chat.complete(
             model=self.model,
             temperature=self.temperature,
             messages=self.conversation_history
         )
+
         return chat_response.choices[0].message.content
+
+    def get_conversation_history (self) :
+        return self.conversation_history
+
+
+    def clear_conversation_history (self) :
+        self.conversation_history = [{"role": "system", "content": system_prompt}]
+        return self.conversation_history
+
+
 
 
 
 def main () :
 
-    response = client.chat.completions.create(
-        model=model,
-        temperature=0.4,
-        messages=[
-            {
-                "role": "user",
-                "content": "What is the capital of France ?"
-            }
-        ]
-    )
+    print("\033[1;34m  I am a helpful assistant!  \033[0m")
 
-    
+    print("Ask me anything!")
+    print("***********************************")
 
-    print(response.choices[0].message.content)
+    discussion = Conversation()
+
+
+    while True :
+        user_imput = input("\033[1;32m User (q/quit | cls/clear |  h/history): \033[0m")
+
+        if user_imput.lower() in ["q", "quit"] :
+            print("\033[1;31m Goodbye! \033[0m")
+            break
+        elif user_imput.lower() in ["cls", "clear"] :
+            discussion.clear_conversation_history()
+            print("\033[1;33m Conversation history cleared! \033[0m")
+
+        elif user_imput.lower() in ["h", "history"] :
+            history = discussion.get_conversation_history()
+            for message in history :
+                role = message["role"]
+                content = message["content"]
+                print(f"\033[1;34m{role.capitalize()}: \033[0m{content}")
+
+        else :
+            response = discussion.new_message(user_imput)
+            print(f"\033[1;35m Assistant: \033[0m{response}")
+
 
 
 if __name__ == "__main__":
